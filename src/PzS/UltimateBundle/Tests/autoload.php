2395f1bc4a8490f5613f67a7de459673a7b9c212
Jim Martens <jim1@live.de>
2013-05-18 20:05:48 +0200

    Initial commit.

---
+++ src/PzS/UltimateBundle/Tests/autoload.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('WCF_PATH')) {
+	// change appropriately
+	define('WCF_PATH', 'E:/Users/Development/AppData/Local/ApacheSoftwareFoundation/Apache2.2/htdocs/Test/WCF2.0x-phpunit/wcf/');
+}
+
+require_once(WCF_PATH.'global.php');
