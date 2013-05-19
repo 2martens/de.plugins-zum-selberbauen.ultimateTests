261634d10616db3629136c63574b11219d128493
Jim Martens <jim1@live.de>
2013-05-19 17:52:05 +0200

    Updated autoload.php.

--- src/PzS/UltimateBundle/Tests/autoload.php
+++ src/PzS/UltimateBundle/Tests/autoload.php
@@ -3,5 +3,16 @@ if (!defined('WCF_PATH')) {
 	// change appropriately
 	define('WCF_PATH', 'E:/Users/Development/AppData/Local/ApacheSoftwareFoundation/Apache2.2/htdocs/Test/WCF2.0x-phpunit/wcf/');
 }
-
-require_once(WCF_PATH.'global.php');
+if (!defined('ULTIMATE_PATH')) {
+	define('ULTIMATE_PATH', 'E:/Users/Development/AppData/Local/Git-repos/de.plugins-zum-selberbauen.ultimate/src/');
+}
+$_SERVER['HTTP_HOST'] = 'localhost';
+$_SERVER['HTTP_ACCEPT'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'de-de,de;q=0.8,en-us;q=0.5,en;q=0.3';
+$_SERVER['HTTP_ACCEPT_ENCODING'] = 'gzip, deflate';
+$_SERVER['HTTP_CONNECTION'] = 'keep-alive';
+$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0';
+$_SERVER['SERVER_SOFTWARE'] = 'Apache/2.2.22 (Win32) mod_ssl/2.2.22 OpenSSL/0.9.8t PHP/5.3.25';
+$_SERVER['DOCUMENT_ROOT'] = 'E:/Users/Development/AppData/Local/ApacheSoftwareFoundation/Apache2.2/htdocs/';
+$_SERVER['REQUEST_URI'] = '/';
+require_once(ULTIMATE_PATH.'global.php');
